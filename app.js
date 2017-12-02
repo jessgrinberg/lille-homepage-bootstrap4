b212a6d71f908f6143020f1b57c2a582f997809b
jessgrinberg <jessgrinberg@gmail.com>
2017-12-02 14:05:28 -0800

    cencentered header

---
+++ app.js
@@ -0,0 +1,26 @@
+$(window).scroll(function() {
+    if($(this).scrollTop()> $( window ).height() ) {
+        $( ".navbar-me" ).addClass("fixed-me");
+    } else {
+        $( ".navbar-me" ).removeClass("fixed-me");
+    }
+});
+
+$('#myCarousel').carousel({
+  interval: 10000
+})
+
+$('.carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  }
+  else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
