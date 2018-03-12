ff75350802fec258eccd3a35befdc0c394647638
jessgrinberg <jessgrinberg@gmail.com>
2018-03-12 16:41:52 -0700

    added header JS

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-$(window).scroll(function() {
-    if($(this).scrollTop()> $( window ).height() ) {
-        $( ".navbar-me" ).addClass("fixed-me");
-    } else {
-        $( ".navbar-me" ).removeClass("fixed-me");
-    }
-});
-
-
 
 (function(){
 
