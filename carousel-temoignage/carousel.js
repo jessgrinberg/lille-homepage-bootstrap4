657f65fe482c0ec8d2c413b61b221cedabc24e82
jessgrinberg <jessgrinberg@gmail.com>
2018-04-12 16:10:56 -0700

    fixed responsive design header

--- carousel-temoignage/carousel.js
+++ carousel-temoignage/carousel.js
@@ -1,19 +1,40 @@
-$('#carouselExample').on('slide.bs.carousel', function (e) {
+$('#carouselThematique').on('slide.bs.carousel', function (e) {
 
     var $e = $(e.relatedTarget);
     var idx = $e.index();
     var itemsPerSlide = 3;
-    var totalItems = $('.carousel-item').length;
+    var totalItems = $('#carouselThematique .carousel-item').length;
 
     if (idx >= totalItems-(itemsPerSlide-1)) {
         var it = itemsPerSlide - (totalItems - idx);
         for (var i=0; i<it; i++) {
             // append slides to end
             if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+                $('#carouselThematique .carousel-item').eq(i).appendTo('#carouselThematique .carousel-inner');
             }
             else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+                $('#carouselThematique .carousel-item').eq(0).appendTo('#carouselThematique .carousel-inner');
+            }
+        }
+    }
+});
+
+$('#carouselTemoignage').on('slide.bs.carousel', function (e) {
+
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $('#carouselTemoignage .carousel-item').length;
+
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('#carouselTemoignage .carousel-item').eq(i).appendTo('#carouselTemoignage .carousel-inner');
+            }
+            else {
+                $('#carouselTemoignage .carousel-item').eq(0).appendTo('#carouselTemoignage .carousel-inner');
             }
         }
     }
